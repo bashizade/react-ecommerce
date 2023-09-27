79f7420e4ab61dee6008cc91f50e913ae282e427
amir bashizade <bashizade.official@gmail.com>
2023-09-27 22:57:31 +0330

    fix navbar bg color

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,21 +1,21 @@
 const Navbar = () => {
     return (
         <div className="flex justify-center w-full">
-            <div className="bg-base-200 w-8/12 rounded-bl-xl rounded-br-xl flex justify-center items-center">
+            <div className="bg-primary w-9/12 rounded-bl-xl rounded-br-xl flex justify-center items-center text-white">
                 <ul className="menu menu-horizontal px-1" dir="rtl">
-                    <li><a>صفحه اصلی</a></li>
+                    <li clas><a>صفحه اصلی</a></li>
                     <li tabIndex={0}>
                         <details>
                             <summary>دسته بندی ها</summary>
                             <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
+                                <li clas><a>Submenu 1</a></li>
+                                <li clas><a>Submenu 2</a></li>
                             </ul>
                         </details>
                     </li>
-                    <li><a>محصولات</a></li>
-                    <li><a>ارتباط با ما</a></li>
-                    <li><a>درباره ما</a></li>
+                    <li clas><a>محصولات</a></li>
+                    <li clas><a>ارتباط با ما</a></li>
+                    <li clas><a>درباره ما</a></li>
                 </ul>
             </div>
         </div>
