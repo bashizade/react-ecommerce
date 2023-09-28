d4d991bcd8feaa509ff92eef78bf6d344e523ddd
bashizade.dev <bashizade.official@gmail.com>
2023-09-28 12:59:01 +0330

    fix

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -3,19 +3,19 @@ const Navbar = () => {
         <div className="flex justify-center w-full">
             <div className="bg-primary bg-opacity-80 backdrop:blur-sm w-9/12 rounded-bl-xl rounded-br-xl flex justify-center items-center text-white">
                 <ul className="menu menu-horizontal px-1" dir="rtl">
-                    <li clas><a>صفحه اصلی</a></li>
+                    <li><a>صفحه اصلی</a></li>
                     <li tabIndex={0}>
                         <details>
                             <summary>دسته بندی ها</summary>
                             <ul className="p-2">
-                                <li clas><a>Submenu 1</a></li>
-                                <li clas><a>Submenu 2</a></li>
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
                             </ul>
                         </details>
                     </li>
-                    <li clas><a>محصولات</a></li>
-                    <li clas><a>ارتباط با ما</a></li>
-                    <li clas><a>درباره ما</a></li>
+                    <li><a>محصولات</a></li>
+                    <li><a>ارتباط با ما</a></li>
+                    <li><a>درباره ما</a></li>
                 </ul>
             </div>
         </div>
